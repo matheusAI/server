83dcb9f887d8073c6db709685c42b96a78552841
matheusAI <ms2279584@gmail.com>
2024-03-03 17:24:15 -0300

    bug

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,6 @@ const mongo = require("mongoose")
 //Configurando o mongoose
     mongo.Promise = Promise.grobal;
     mongo.connect("mongodb://localhost/Users", {
-        userMongoClient: true
     }).then(() => {
         console.log("mongodb conectador com sucesso")
     }).catch((err) => {
